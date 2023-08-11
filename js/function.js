66c587bc2774780e92ea5eee1e9e1a51850b346b
이두 <dlgkrwls05@gmail.com>
2023-08-11 12:43:19 +0900

    전체 1차작성

--- js/function.js
+++ js/function.js
@@ -1,1 +1,39 @@
-$
+const $indicator = $('ul>li');
+const $bgImg = $('.mainimg');
+let nowIdx = 0;
+
+$indicator.on('click', function (evt) {
+	evt.preventDefault();
+
+	nowIdx = $indicator.index(this);
+
+	$bgImg.animate({ marginLeft: -478 * nowIdx + 'px' });
+	$indicator.eq(nowIdx).addClass('on');
+	$indicator.eq(nowIdx).siblings().removeClass('on');
+});
+
+$('.next').on('click', function (evt) {
+	evt.preventDefault();
+
+	if (nowIdx < $indicator.length - 1) {
+		nowIdx++;
+	} else {
+		nowIdx = 0;
+	}
+	$bgImg.animate({ marginLeft: -478 * nowIdx + 'px' });
+	$indicator.eq(nowIdx).addClass('on');
+	$indicator.eq(nowIdx).siblings().removeClass('on');
+});
+
+$('.prev').on('click', function (evt) {
+	evt.preventDefault();
+
+	if (nowIdx > 0) {
+		nowIdx--;
+	} else {
+		nowIdx = $indicator.length - 1;
+	}
+	$bgImg.animate({ marginLeft: -478 * nowIdx + 'px' });
+	$indicator.eq(nowIdx).addClass('on');
+	$indicator.eq(nowIdx).siblings().removeClass('on');
+});
